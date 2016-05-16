abd62d89dbb427d99ace25cc310843b9c1ba05aa
Marius Vollmer <mvollmer@redhat.com>
2016-05-16 16:17:44 +0300

    util: Add support for reading bourne shell files

--- tests/unit/test_util_sh.py
+++ tests/unit/test_util_sh.py
@@ -50,11 +50,42 @@ class TestAtomicUtilSh(unittest.TestCase):
         self.assertEqual(util.sh_modify_var_in_text('VAR=OLD\n', "VAR", uppercasify),
                          'VAR=OLD\n\nVAR=""\n')
 
+    def test_util_sh_get_text(self):
+        # Non-existing setting returns default
+        self.assertEqual(util.sh_get_var_in_text('', "VAR", "def"),
+                         "def")
+        self.assertEqual(util.sh_get_var_in_text('FOO="bar"\n', "VAR", "def"),
+                         "def")
+
+        # Existing setting will be found
+        self.assertEqual(util.sh_get_var_in_text('VAR="val"\n', "VAR"),
+                         "val")
+
+        # Two settings will return the last
+        self.assertEqual(util.sh_get_var_in_text('VAR="val1"\nVAR="val2"\n', "VAR"),
+                         "val2")
+
+        # Setting on partial line is recognized
+        self.assertEqual(util.sh_get_var_in_text('VAR="val"', "VAR"),
+                         "val")
+
+        # Setting with extra whitespace is recognized
+        self.assertEqual(util.sh_get_var_in_text('   VAR  =  "val"  \n', "VAR"),
+                         "val")
+
+        # Setting in a comment is not recognized
+        self.assertEqual(util.sh_get_var_in_text('# VAR="val"\n', "VAR", "def"),
+                         "def")
+
+        # Setting without quotes around the value is not recognized
+        self.assertEqual(util.sh_get_var_in_text('VAR=val\n', "VAR", "def"),
+                         "def")
+
     def assertFileEqual(self, file, content):
         self.assertEqual(open(file, "r").read(), content)
 
     def test_util_sh_modify_file(self):
-        file = os.path.join(os.environ["WORK_DIR"], "sh.conf")
+        file = os.path.join(os.environ["WORK_DIR"], "sh1.conf")
 
         def uppercasify(old):
             return old.upper()
@@ -69,6 +100,20 @@ class TestAtomicUtilSh(unittest.TestCase):
             f.write('VAR="val"\n')
         util.sh_modify_var_in_file(file, "VAR", uppercasify)
         self.assertFileEqual(file, 'VAR="VAL"\n')
+
+    def test_util_sh_get_file(self):
+        file = os.path.join(os.environ["WORK_DIR"], "sh2.conf")
+
+        # Non-existing file is treated as empty
+        self.assertFalse(os.path.exists(file))
+        self.assertEqual(util.sh_get_var_in_file(file, "VAR", "def"),
+                         "def")
+
+        # Existing file is scanned
+        with open(file, "w") as f:
+            f.write('VAR="val"\n')
+        self.assertEqual(util.sh_get_var_in_file(file, "VAR"),
+                         "val")
 
 if __name__ == '__main__':
     unittest.main()
